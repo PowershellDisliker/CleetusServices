1b900854467fd7299580096a5552971d98dea0a7
Ryley Turner <losycresent@gmail.com>
2026-08-19 13:37:31 -0500

    storing saves

--- project-zomboid/entrypoint.sh
+++ project-zomboid/entrypoint.sh
@@ -2,11 +2,7 @@
 set -e
 
 echo "Syncing missing game files to mounted drive..."
-# -a: archive mode (preserves permissions/times)
-# -v: verbose
-# --ignore-existing: skip updating files that already exist in target
 rsync -av --ignore-existing /game_files/ /data/
 
 echo "Starting server..."
-# Execute the start script passing any CMD arguments
 exec /data/start-server.sh "$@"
