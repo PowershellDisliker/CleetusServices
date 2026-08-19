f2104e56755cb10701471556ca5cfc2488db17fd
Ryley Turner <losycresent@gmail.com>
2026-08-19 12:24:42 -0500

    persistent storage for zomboid

---
+++ project-zomboid/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Syncing missing game files to mounted drive..."
+# -a: archive mode (preserves permissions/times)
+# -v: verbose
+# --ignore-existing: skip updating files that already exist in target
+rsync -av --ignore-existing /game_files/ /data/
+
+echo "Starting server..."
+# Execute the start script passing any CMD arguments
+exec /mounted_drive/start-server.sh "$@"
